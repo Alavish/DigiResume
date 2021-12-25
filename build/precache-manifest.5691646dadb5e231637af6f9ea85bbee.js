6687b049b2a7a96694f3e2c969176554f407cb79
Alavish <achristopher22@my.whitworth.edu>
2021-12-25 01:59:10 -0800

    New

---
+++ build/precache-manifest.5691646dadb5e231637af6f9ea85bbee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417614f5ca80edda5233809736c188bf",
+    "url": "/SCLeoo/resumeindex.html"
+  },
+  {
+    "revision": "b9b52b0d49a7f7ec9668",
+    "url": "/SCLeoo/resumestatic/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/SCLeoo/resumestatic/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/SCLeoo/resumestatic/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b52b0d49a7f7ec9668",
+    "url": "/SCLeoo/resumestatic/js/main.18996e48.chunk.js"
+  },
+  {
+    "revision": "ca9ef760eba73ecf75c5",
+    "url": "/SCLeoo/resumestatic/js/runtime-main.ba429645.js"
+  }
+]);
