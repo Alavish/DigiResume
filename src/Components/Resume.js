32fada576ea252930ddeabc56c759d96b387300c
Alavish <achristopher22@my.whitworth.edu>
2023-11-17 12:09:52 -0800

    Testing for <p> tag invisibility

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -8,18 +8,13 @@ class Resume extends Component {
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-       </div>
+        <p>{education.description}</p></div>
       })
       var work = this.props.data.work.map(function(work){
         return <div key=
         {work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.DitemA}</p>
-            <p>{work.DitemB}</p>
-            <p>{work.DitemC}</p>
-            <p>{work.DitemD}</p>
-            <p>{work.DitemE}</p>
-            <p>{work.DitemF}</p>
+            <p>{work.description}</p>
             <img class = "logo" src = {work.logo}></img>
             <br></br>
             <br></br>
