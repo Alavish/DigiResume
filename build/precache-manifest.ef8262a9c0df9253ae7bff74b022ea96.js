9a5d707ebdd670aadc9f938ba8fd89d67fa33814
“Alavish” <achristopher22@my.whitworth.edu>
2023-12-22 00:41:30 -0800

    Favicon

---
+++ build/precache-manifest.ef8262a9c0df9253ae7bff74b022ea96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf08dcbb3ce2b7c99916d602c3f129a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7766437031cbc940670f",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "fbb4e0c7bd741cfc7351",
+    "url": "/static/js/2.c56b5c2c.chunk.js"
+  },
+  {
+    "revision": "88a1e5fde3fbde07a43dac26e09075cb",
+    "url": "/static/js/2.c56b5c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7766437031cbc940670f",
+    "url": "/static/js/main.b5545583.chunk.js"
+  },
+  {
+    "revision": "9782e4b201e91e52ccd4",
+    "url": "/static/js/runtime-main.e4c0243c.js"
+  }
+]);
